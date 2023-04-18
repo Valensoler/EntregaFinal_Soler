e7f1e46397d7b63640371deee398cb7355837671
Valensoler <solervalentina6@gmail.com>
2023-04-18 00:08:08 -0300

    categories y navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,44 +1,58 @@
 import './NavBar.css'
 import CartWidget from '../CartWidget/CartWidget'
 import { Link, NavLink, useParams } from "react-router-dom"
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 // import { collection, getDocs, query, orderBy, where } from 'firebase/firestore'
 // import { db } from "../../service/firebase/firebaseConfig"
 import { getCategories } from '../../service/firebase/firestore/categories'
 import { useAsync } from '../../hooks/useAsync'
 
 const NavBar = () => {
-    const { categorieId } = useParams ()
+        // const [categories, setCategories] = useState([])
+      
+        // useEffect(() => {
+        //   const categoriesRef = query(collection(db, 'categories'), orderBy('order'))
+      
+        //    getDocs(categoriesRef)
+        //     .then(snapshot => {
+        //         const categoriesAdapted = snapshot.docs.map(doc => {
+        //           const data = doc.data()
+      
+        //           return { id: doc.id, ...data }
+        //         })
+      
+        //         setCategories(categoriesAdapted)
+        //     }).catch(error => {
+        //       return error
+        //     })
+        // }, [])
+      
+    const { categoriesId } = useParams ()
 
-    const getCategoriesWithId = () => getCategories (categorieId)
+    const getCategoriesWithId = () => getCategories (categoriesId)
 
-    const { data: categories} = useAsync(getCategoriesWithId, [categorieId])
-// const [categories, setCategories] = useState([])    
-//   useEffect(() => {
-//     const categoriesRef = categorieId
-//     ? query(collection(db, 'categories'), where ('categories', '==', 'categorieId'))
-//     : collection (db,'categories')
-//     orderBy('order')
+    const { data: categories, error, loading} = useAsync(getCategoriesWithId, [categoriesId])
 
-//     getDocs(categoriesRef)
-//         .then(snapshot => {
-//           const categoriesAdapted = snapshot.docs.map(doc => {
-//             const data = doc.data()
-//             return { id: doc.id, ...data }
-//           })
+    if(loading) {
+        return <h1>Cargando...</h1>
+    }
 
-//           setCategories(categoriesAdapted)
+    if(error) {
+        return <h1>Vuelva a cargar la pagina</h1>
+    }
 
-//       }).catch(error => {
-//             return error
-//       })
-//   }, [])
 
   return (
     <nav className="NavBar" >
         <Link to='/' className='Titulo'>Donna Civetta</Link>
         <div className="Categories">
-            <NavLink key={categories.id} to={`/category/${categories.slug}`} className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>{categories.label}</NavLink>
+        {
+            categories.map(cat => {
+                return (
+                  <NavLink key={cat.id} to={`/category/${cat.slug}`} className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>{cat.label}</NavLink>
+                )
+              }) 
+        }
             <CartWidget/>
         </div>
     </nav>
--- src/service/firebase/firestore/categories.js
+++ src/service/firebase/firestore/categories.js
@@ -1,43 +1,44 @@
 import { db } from "../firebaseConfig"
-import { collection, getDoc, doc } from "firebase/firestore"
+import { collection, getDoc, doc} from "firebase/firestore"
 
-
-// export const getCategories = (categorieId) => {
-   
-//     const collectionCat = collection(db,"categories")
-
-//     const categoriesRef = doc(collectionCat,itemId)
-
-//     return getDoc(categoriesRef)
-//     .then(result => {
-//             const categoriesAdapted = {
-//                 id:result.id,
-//                 ...result.data()
-//               }
-        
-//             return categoriesAdapted
-//         })
-//         .catch(error => {
-//             return error
-//         })
-// }
 
 export const getCategories = (categorieId) => {
+   
+    const collectionCat = collection(db,"categories")
 
-    const categoriesRef = categorieId 
-    ? query(collection(db, 'categories'), where('categories', '==', categorieId))
-    : collection(db, 'categories')
+    const categoriesRef = doc(collectionCat,categorieId)
 
     return getDoc(categoriesRef)
-        .then(snapshot => {
-          const categoriesAdapted = snapshot.docs.map(doc => {
-            const data = doc.data()
-            return { id: doc.id, ...data }
-          })
-
-          return categoriesAdapted
-
-      }).catch(error => {
+    .then(result => {
+            const categoriesAdapted = {
+                id:result.id,
+                ...result.data()
+              }
+        
+            return categoriesAdapted
+        })
+        .catch(error => {
             return error
-      })
+        })
 }
+
+// export const getCategories = (categoriesId) => {
+
+//     const categoriesRef = categoriesId 
+//     ? query(collection(db, 'categories'), where('categories', '==', categoriesId))
+//     : collection(db, 'categories')
+//     console.log (categoriesId)
+
+//     return getDoc(categoriesRef)
+//         .then(snapshot => {
+//           const categoriesAdapted = snapshot.docs.map(doc => {
+//             const data = doc.data()
+//             return { id: doc.id, ...data }
+//           })
+
+//           return categoriesAdapted
+
+//       }).catch(error => {
+//             return error
+//       })
+// }
